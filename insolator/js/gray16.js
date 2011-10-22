e5d194feff532c48e28a0b524c375e630c6313f6
Adam Wight <spam@ludd.net>
2011-10-22 10:40:08 -0700

    reimp of shadows using a raytracer and 64-bit grayscale

---
+++ insolator/js/gray16.js
@@ -0,0 +1,17 @@
+Gray64 = function(xres, yres)
+{
+  var m = new Array(xres * yres);
+  function downsample()
+  {
+    return rgb24;
+  }
+
+  function add_fraction(image, divisor = 0x10000)
+  {
+  }
+
+  function maximum(image)
+  {
+    return max_value;
+  }
+};
---
+++ insolator/js/ui.js
@@ -0,0 +1,13 @@
+progress_renderer(percent)
+{
+    pacman(percent);
+}
+    
+day_slider = function()
+{
+    return date;
+}
+
+
+hour_slider
+
